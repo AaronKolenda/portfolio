b615531df3395e83564096235e5975d447b191cc
AaronKolenda <kolenda.a@gmail.com>
2015-04-27 16:19:51 -0400

    improving styling

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 $(document).on("ready", function() {
 
 	var $nav = $("#nav");
+	var $nava = $("#nav a");
 
 	$nav.sticky({topSpacing:0});
 
@@ -8,13 +9,15 @@ $(document).on("ready", function() {
 		console.log("in scroll");
 		if ($("#nav-sticky-wrapper").hasClass('is-sticky')) {
 			console.log("in if");
-			$nav.css("background-color", "rgba(255, 255, 255, .9");
+			$nav.css("background-color", "rgba(119, 31, 31, 0.93)");
+			$nava.css("color", "rgb(255, 209, 220)");
 			$nav.css("top", "-40px");
 			$nav.css("z-index", "1");
 		}
 		if ($("#nav-sticky-wrapper").hasClass('is-sticky') === false) {
 			console.log("in second if");
 			$nav.css("background-color", "transparent");
+			$nava.css("color", "red");
 			$nav.css("top", "0px");
 			$nav.css("z-index", "0");
 		}
